cb071802168cf85ef22e5e3373aab97c1e33ad68
Emir Selver <emirselver1@hotmail.com>
2024-08-31 23:23:47 +0300

    update numpy notes

---
+++ Makine Öğrenmesi Ön Hazırlık/ders1.py
@@ -0,0 +1,11 @@
+"""
+numpy -> numerical python
+numpy array python listelere göre daha dinamik/kullanışlıdır
+daha hızlı çalışmaktadır
+"""
+
+import numpy as np
+
+meyve_dizisi = np.array(["elma", "armut", "muz"])
+
+print(meyve_dizisi)
